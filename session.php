09d7f451b921045bfdd1721908cc6842524a8587
sebgates <sebgates@gmail.com>
2019-03-01 15:38:59 +0000

    Initial Commit

---
+++ session.php
@@ -0,0 +1,20 @@
+Example
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+    <a href="session2.php">Go to next page</a>
+    
+</body>
+</html>
